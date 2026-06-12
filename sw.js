4b18e21fd119035b3749eb1b1ab97b57973cbaca
Ephrem Dushimimana <your-github-email@gmail.com>
2026-06-12 12:05:37 +0200

    chore: Increment cache version to v2 and add activate listener to bust stale cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'beyond-justice-v1';
+const CACHE_NAME = 'beyond-justice-v2';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -10,6 +10,21 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Clearing old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
   );
 });
 
